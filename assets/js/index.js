c9df20b0ea0d40db74409c30497e3d922993c17c
RKH1988 <regettaspearman@gmail.com>
2022-07-06 21:31:48 -0400

    added color coding based on past present and future

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,38 @@
+//empty array to save in local storage
 var tasks = {};
 
+//hour for current time
+var currHr = parseInt(moment().format("H"));
+
+//block that changes color
+var schedule = $(".schedule-item");
+
+var saveBtn = $(".save");
+
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+var colorChange= function() {
+    schedule.each(function (i, schedule) {
+        var i = parseInt($(this).attr("id"));
+        if(currHr > i){
+            $(schedule).removeClass("bg-secondary")
+            .removeClass("future")
+            .removeClass("present")
+            .addClass("past");
+        }
+        else if(currHr === i) {
+            $(schedule).removeClass("bg-secondary")
+            .removeClass("future")
+            .removeClass("past")
+            .addClass("present");
+        }
+        else if(currHr < i) {
+            $(schedule).removeClass("bg-secondary")
+            .removeClass("past")
+            .removeClass("present")
+            .addClass("future");
+        };
+    });
+}
+
+colorChange();
