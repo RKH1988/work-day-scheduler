b75b3709f21c1d29c937f73d29459aba70217408
RKH1988 <regettaspearman@gmail.com>
2022-07-06 23:10:32 -0400

    retrieve from local storage on page load

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,8 +4,10 @@ var currHr = parseInt(moment().format("H"));
 //block that changes color
 var schedule = $(".schedule-item");
 
+//identifies the save class where the button is held
 var saveBtn = $(".save");
 
+//display current day at top of page
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 var colorChange= function() {
@@ -29,8 +31,10 @@ var colorChange= function() {
     });
 }
 
+// update the color change function every second
 setInterval(colorChange(),1000);
 
+//save to local storage
 saveBtn.on("click", function(event) {
     var taskId = $(this).attr("id");
     console.log(taskId);
@@ -39,3 +43,17 @@ saveBtn.on("click", function(event) {
 
     localStorage.setItem(taskText.attr("id"), taskText.val());
 });
+
+//retrieve from local storage on page load
+$("#schedule-item8").text(localStorage.getItem("schedule-item8"));
+$("#schedule-item9").text(localStorage.getItem("schedule-item9"));
+$("#schedule-item10").text(localStorage.getItem("schedule-item10"));
+$("#schedule-item11").text(localStorage.getItem("schedule-item11"));
+$("#schedule-item12").text(localStorage.getItem("schedule-item12"));
+$("#schedule-item13").text(localStorage.getItem("schedule-item13"));
+$("#schedule-item14").text(localStorage.getItem("schedule-item14"));
+$("#schedule-item15").text(localStorage.getItem("schedule-item15"));
+$("#schedule-item16").text(localStorage.getItem("schedule-item16"));
+$("#schedule-item17").text(localStorage.getItem("schedule-item17"));
+
+
